13d455248d704cc701858bf6278e8eebbb13904a
CzTwo <17730127131@163.com>
2018-06-12 17:50:14 +0800

    modify personal information

---
+++ src/components/common/information/information.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="information">
+    <header class="information-header">
+      <div class="header-portrait">
+        <img src="../../../../static/img/default-icon.png">
+      </div>
+      <div class="header-content">
+        <p class="header-name">曹俊5</p>
+        <p class="header-account">账号:17730127131</p>
+      </div>
+      <div class="close-button" @click="closeInformation">
+        <img src="../../../../static/img/closebtn.png">
+      </div>
+    </header>
+    <section class="information-section">
+      <p class="section-title">基本信息</p>
+      <div class="title-list">
+        <div class="list-item">
+          <span class="item-title">生日</span>
+          <span class="item-content">--</span>
+        </div>
+        <div class="list-item">
+          <span class="item-title">手机</span>
+          <span class="item-content">--</span>
+        </div>
+        <div class="list-item">
+          <span class="item-title">邮箱</span>
+          <span class="item-content">--</span>
+        </div>
+        <div class="list-item">
+          <span class="item-title">签名</span>
+          <span class="item-content">--</span>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+      closeInformation() {
+        this.$emit('closeInformation');
+      }
+    }
+  }
+
+</script>
+<style scoped lang="scss">
+  @import '../../scss/information/information.scss';
+
+</style>
---
+++ src/components/common/shade/shade.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="box"></div>
+</template>
+<script>
+  export default {
+
+  }
+
+</script>
+<style scoped lang="scss">
+  @import '../../scss/shade/shade.scss';
+
+</style>
