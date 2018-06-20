9d87c5f87d9e4c81a0013bed5fe80a2285c6393c
CzTwo <17730127131@163.com>
2018-06-20 18:06:28 +0800

    modify main interface

--- src/components/user-menu/contact/contact.vue
+++ src/components/user-menu/contact/contact.vue
@@ -3,11 +3,25 @@
     <nav class="contact-nav">
       <div class="nav-item" @click="selectList('session')">
         <i></i>
-        <div class="item-arrow" v-show="listShow == 'session'"></div>
+        <div class="item-arrow" v-show="listShow == 'session'">
+          <div class="badge-bottom">
+            <b class="badge-top">
+              <i class="top-arrow-first"></i>
+              <i class="top-arrow-second"></i>
+            </b>
+          </div>
+        </div>
       </div>
       <div class="nav-item"  @click="selectList('friend')">
         <i></i>
-        <div class="item-arrow" v-show="listShow == 'friend'"></div>
+        <div class="item-arrow" v-show="listShow == 'friend'">
+          <div class="badge-bottom">
+            <b class="badge-top">
+              <i class="top-arrow-first"></i>
+              <i class="top-arrow-second"></i>
+            </b>
+          </div>
+        </div>
       </div>
     </nav>
     <SessionList v-show="listShow == 'session'"></SessionList>
